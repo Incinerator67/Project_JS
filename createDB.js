1b93a70e1f5adff6925c1b69c28ec8fe3621c8b0
Edem Osmanov <edemosmanov2699@gmail.com>
2022-11-18 20:31:58 +0300

    создал БД и МД

---
+++ createDB.js
@@ -0,0 +1,19 @@
+var MongoClient = require('mongodb').MongoClient
+var data = require("./data.js").data
+const uri = "mongodb://localhost:27017/"
+const client = new MongoClient(uri)
+async function run() {
+try {
+await client.connect();
+var database = client.db("Subnautica");
+database.dropDatabase()
+database = client.db("Subnautica");
+const fauna = database.collection("fauna_Sub");
+const result = await fauna.insertOne({name:"Warper"});
+console.log(`${result} documents were inserted`);
+} finally {
+await client.close();
+}
+}
+run()
+
