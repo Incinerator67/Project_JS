a990858ea26de9e3fc08bddaebafaf6c7173a888
Edem Osmanov <edemosmanov2699@gmail.com>
2022-11-25 10:56:39 +0300

    добавили маршрутизатор и подключило БД

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var Leviathan = require('./routes/Leviathan');
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1/Subnautica')
 
 var app = express();
 
@@ -22,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/Leviathan', Leviathan);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
