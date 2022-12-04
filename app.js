9350d772bebfcd0841595fe57a7cbefdd42f29a4
Edem Osmanov <edemosmanov2699@gmail.com>
2022-12-04 19:57:17 +0300

    created a counter of visiting of the site

--- app.js
+++ app.js
@@ -30,7 +30,10 @@ app.use(session({
   saveUninitialized: true	,
   store: MongoStore.create({mongoUrl: 'mongodb://127.0.0.1:27017/Subnautica'})
 }))
-
+app.use(function(req,res,next){
+  req.session.counter = req.session.counter +1 || 1
+  next()
+})
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Leviathan', Leviathan);
